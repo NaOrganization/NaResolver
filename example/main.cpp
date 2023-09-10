6ba68099ac4ac710b3b815f3291571e8e8ffbf4b
MidTerm <83219310+MidTerm-CN@users.noreply.github.com>
2023-09-10 17:20:44 +0800

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -4,12 +4,40 @@
 
 #define LOGGER_FUNCTION(level) ([](std::string m, ...)->void\
 			{\
-			m = #level " " + m + "\n";\
+			m = #level " [NaResolver] " + m + "\n";\
 			va_list args;\
 			va_start(args, m);\
 			vprintf(m.c_str(), args);\
 			va_end(args);\
 			})
+
+// per-register the class, it will init in init period
+// the first parameter: Assembly
+// secondly: namespace
+// thirdly: class name ( including declarative name of class )
+// fourth: inherit ( pay attention: you need have a ":" before your inheritable class )
+NA_RESOLVER_REGISTER_CLASS(UnityEngine.CoreModule, UnityEngine, GameObject, : public std::string)
+{
+public:
+	void* get_transform();
+};
+
+// in a similar way
+// firstly: which class's namespace it comes from
+// secondly: which class it comes from
+// thirdly: signature
+// fourth: return type
+// fifth: function name
+// ...: parameters (type)
+NA_RESOLVER_REGISTER_METHOD(UnityEngine, GameObject, "UnityEngine.Transform get_transform()", void*, get_transform, )
+{
+	// __{ namespace }_{ class }_Class_{ function }_Method__
+	// As<{ return type }, { self class }, { the last parameter must is a MethodInfo }, ...>()
+	// Attention: If this function's return type is the void, you need to ues the VoidInvode.
+	__UnityEngine_GameObject_Class_get_transform_Method__.As<void*, GameObject*, GeneralMethodInfo*>().
+		Invoke(this, nullptr);
+}
+// Suggestion: add the function in cpp
 
 int WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -18,24 +46,26 @@ int WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 		DisableThreadLibraryCalls(hinstDLL);
 		AllocConsole();
 		freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
-		NaResolver::Config config;
-		config.enableLogger = true;
-		config.logger.debug = LOGGER_FUNCTION([DEBUG]);
-		config.logger.info = LOGGER_FUNCTION([INFO]);
-		config.logger.error = LOGGER_FUNCTION([ERROR]);
-		config.logger.fatal = LOGGER_FUNCTION([FATAL]);
+		try
+		{
+			NaResolver::Config config;
+			config.enableLogger = true;
+			config.logger.debug = LOGGER_FUNCTION([Debug]);
+			config.logger.info = LOGGER_FUNCTION([Info]);
+			// i delete the error and fatal log, there will be export with the NaResolver::Exception
 
-		if (!Il2CppResolver->Setup(config))
-			printf("Failed to setup I2Hrame!\n");
-		Il2CppClass* klass = Il2CppResolver->GetClassEx("UnityEngine.CoreModule", "UnityEngine", "GameObject");
-		printf("(Il2CppResolver->GetClassEx) Class: %p\n", klass);
-		klass = Il2CppResolver->GetClass("(UnityEngine.CoreModule)UnityEngine.GameObject");
-		printf("(Il2CppResolver->GetClass) Class: %p\n", klass);
-		Il2CppMethodPointer method = Il2CppResolver->GetMethod(klass, "UnityEngine.Transform get_transform()");
-		printf("(Il2CppResolver->GetMethod) Method: %p\n", method);
+			if (!Il2CppResolver->Setup(config))
+				printf("Failed to setup I2Hrame!\n");
+		}
+		catch (const NaResolver::Exception& e)
+		{
+			// Everything registered will be handled in Setup, so here is a 'try' to catch the error
+			printf("[NaResolver] %s\n", e.message.c_str());
+		}
 
-		klass = Il2CppResolver->GetClass("(UnityEngine.CoreModule)UnityEngine.GameObjct");
-		printf("(Error Demonstration) Class: %p\n", klass);
+		// content is ptr of it ( all same )
+		printf("(Il2CppResolver->GetClass) Class: %p\n", __UnityEngine_GameObject_Class__.content);
+		printf("(Il2CppResolver->GetMethod) Method: %p\n", __UnityEngine_GameObject_Class_get_transform_Method__.content);
 
 		Il2CppResolver->Destroy();
 		free(Il2CppResolver);
